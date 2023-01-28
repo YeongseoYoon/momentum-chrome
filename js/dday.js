ffcfed0afd6ce13d5d5bc26badcc5e1132ba34a1
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-01-29 05:32:52 +0900

    Update dday

--- js/dday.js
+++ js/dday.js
@@ -1,28 +1,49 @@
 const clockTitle = document.querySelector(".dday-subtitle");
 const ddaySubmitBtn= document.querySelector(".dday-submit");
+const ddayDate = document.getElementById("dday-date");
+const ddayForm = document.querySelector(".dday-form");
 const dTitle= document.querySelector(".dday-user-title");
-const ddayDate= document.querySelector(".dday-date").value;
 const nowDate = new Date();
 
-function setClock() {
-  const dDiff = eveDate.getTime() - nowDate.getTime();
-  const dDate = Math.round(dDiff / (1000 * 60 * 60 * 24));
-  clockTitle.innerText = `${dTitle}까지 ${dDate}일 남았습니다.`;
-}
+const DDAY_KEY = "dday";
+const DDAY_TITLE = "ddaytitle";
 
-function handlerSubmitDday(event){
-    event.preventDefault();
-    console.log(ddayDate);
-    console.log(nowDate);
-    if(ddayDate>=nowDate){
-        setClock();
-        setInterval(setClock, 1000);
-    }else{
-        alert("D-Day는 현재보다 미래");
+function changeDate(e){
+    let selectDate = e.target.value;
+    if(new Date(selectDate)<=nowDate){
+        alert("현재 이후의 날짜를 입력해주세요!");
     }
-
 }
 
-ddaySubmitBtn.addEventListener("submit", handlerSubmitDday);
+function setDday(event){
+    event.preventDefault();
+    ddayForm.classList.add("hidden");
+    localStorage.setItem(DDAY_TITLE,dTitle.value);
+    localStorage.setItem(DDAY_KEY,ddayDate.value);
+    paintingDday();
+    setInterval(paintingDday,1000);
+}
+
+function paintingDday(){
+    const toDate = localStorage.getItem(DDAY_KEY);
+    const toTitle = localStorage.getItem(DDAY_TITLE);
+    const dDiff = toDate.getTime() - nowDate.getTime();
+    const dDate = Math.round(dDiff / (1000 * 60 * 60 * 24));
+    clockTitle.innerText = `${toTitle}까지 ${dDate}일 남았습니다.`;
+}
+  
+
+const ddayTitle = localStorage.getItem(DDAY_TITLE);
+
+if(ddayTitle === null){   
+    ddayDate.addEventListener("change", changeDate);
+    ddaySubmitBtn.addEventListener("submit",setDday);
+}else{
+    paintingDday();
+    setInterval(paintingDday,1000);
+}
+
+
+
 
 
