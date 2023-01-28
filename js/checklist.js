4a0a98ff2a6e53cb753c59813f12a950909165f9
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-01-29 01:44:10 +0900

    Update checklist.js

--- js/checklist.js
+++ js/checklist.js
@@ -1,16 +1,6 @@
 let todoList = document.querySelector('#todo-list');
-const checkBox = todoList.querySelector("input[type='checkbox']");
-const spanId = checkBox.nextElementSibling;
-console.dir(spanId);
+const checkBox = todoList.querySelectorAll("input[type='checkbox']");
 
-// const onchange = c => c.nextElementSibling.style.textDecoration = c.checked ? 'line-through' : 'none';
-// Array.from(todoList.querySelectorAll('input[type="checkbox"]'))
-//    .forEach(c => c.onchange = e => onchange(c));
-
-checkBox.addEventListener('change', (event) => {
-   if(checkBox.checked){
-      spanId.style.textDecorationLine = "line-through"
-   }else{
-      spanId.style.textDecorationLine = "none"
-   }
-});
+const onchange = c => c.nextElementSibling.style.textDecoration = c.checked ? 'line-through' : 'none';
+Array.from(todoList.querySelectorAll('input[type="checkbox"]'))
+   .forEach(c => c.onchange = e => onchange(c));
